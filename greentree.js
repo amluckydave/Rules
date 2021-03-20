0b38858a87306be88777db28f87d09232499c13d
徐执默 <45121260+eliotxu@users.noreply.github.com>
2021-03-20 15:37:57 +0800

    Update greentree.js

--- greentree.js
+++ greentree.js
@@ -1,8 +1,3 @@
-// let obj = JSON.parse($response.body);
-// obj.responseData.advertisingList = [];
-// $done({body: JSON.stringify(obj)});
-
-// 两种方案
-$done({ 
-body: $response.body.replace(/(?<=advertisingList"\:)(.*)\]/, "[]")
-})
+let obj = JSON.parse($response.body);
+obj.responseData.advertisingList = [];
+$done({body: JSON.stringify(obj)});
